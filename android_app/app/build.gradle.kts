a062b61ec74a26014c08100e2af2878efca0dd98
KESHUIN0 <keshuin0@gmail.com>
2026-05-04 22:14:42 -0400

    Auto-sync update

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.shift.core"
     compileSdk {
-        version = release(36) {
+        version = release(35) {
             minorApiLevel = 1
         }
     }
